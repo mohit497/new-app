8ab826fcece1d61529da6b75225db3d4562ace4c
mohit <mohit.official497@gmail.com>
2022-01-18 16:35:29 +0700

    wip(card): add mock cards

---
+++ src/components/Home.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app-bar  dark color="blue" elevation="4">
+    <v-toolbar-title >News App</v-toolbar-title>
+  </v-app-bar>
+  <v-container>
+    <v-row class="text-center my-2">
+      <v-col cols="3">
+        <NewsCard />
+      </v-col>
+      <v-col cols="3">
+        <NewsCard />
+      </v-col>
+      <v-col cols="3">
+        <NewsCard />
+      </v-col>
+      <v-col cols="3">
+        <NewsCard />
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import logo from "../assets/logo.svg";
+import NewsCard from "./NewsCard.vue";
+
+export default {
+  name: "HelloWorld",
+  components: {
+    NewsCard,
+  },
+  NewsCarddata: () => ({
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader",
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify",
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
+      },
+    ],
+    importantLinks: [
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com",
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify",
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs",
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify",
+      },
+    ],
+    logo,
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com",
+      },
+      {
+        text: "Roadmap",
+        href: "https://vuetifyjs.com/introduction/roadmap/",
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+      },
+    ],
+  }),
+};
+</script>
--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -2,7 +2,7 @@
   <v-card class="mx-auto" max-width="344" outlined>
     <v-list-item three-line>
       <v-list-item-content>
-        <div class="text-overline mb-4">OVERLINE</div>
+        <div class="text-left mb-2">OVERLINE</div>
         <v-list-item-title class="text-h5 mb-1"> Headline 5 </v-list-item-title>
         <v-list-item-subtitle
           >Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle
