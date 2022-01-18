4fccc800400147fa83f406b6cf2dcfd8efdf549e
mohit <mohit.official497@gmail.com>
2022-01-18 16:25:18 +0700

    feat(header): add header

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,38 @@
 <template>
+  <v-app-bar color="blue" elevation="4">
+    <v-toolbar-title color="white">News App</v-toolbar-title>
+  </v-app-bar>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="logo"
-          class="my-3"
-          contain
-          height="200"
-        />
+      <v-col cols="3">
+        <NewCard />
+      </v-col>
+      <v-col cols="3">
+        <NewCard />
+      </v-col>
+      <v-col cols="3">
+        <NewCard />
+      </v-col>
+      <v-col cols="3">
+        <NewCard />
       </v-col>
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          <div>Welcome to the Vuetify 3 Alpha</div>
+          <div>My News Application</div>
         </h1>
-
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          <br />please join our online
+          <a href="https://community.vuetifyjs.com" target="_blank"
+            >Discord Community</a
+          >
         </p>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          What's next?
-        </h2>
+      <v-col class="mb-5" cols="12">
+        <h2 class="headline font-weight-bold mb-5">What's next?</h2>
 
         <v-row justify="center">
           <a
@@ -47,13 +47,8 @@
         </v-row>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Important Links
-        </h2>
+      <v-col class="mb-5" cols="12">
+        <h2 class="headline font-weight-bold mb-5">Important Links</h2>
 
         <v-row justify="center">
           <a
@@ -68,13 +63,8 @@
         </v-row>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Ecosystem
-        </h2>
+      <v-col class="mb-5" cols="12">
+        <h2 class="headline font-weight-bold mb-5">Ecosystem</h2>
 
         <v-row justify="center">
           <a
@@ -93,59 +83,62 @@
 </template>
 
 <script>
-import logo from '../assets/logo.svg'
+import logo from "../assets/logo.svg";
+import NewCard from "./NewCard.vue";
 
 export default {
-  name: 'HelloWorld',
-
-  data: () => ({
+  name: "HelloWorld",
+  components: {
+    NewCard,
+  },
+  NewCarddata: () => ({
     ecosystem: [
       {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader",
       },
       {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify",
       },
       {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
       },
     ],
     importantLinks: [
       {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
+        text: "Chat",
+        href: "https://community.vuetifyjs.com",
       },
       {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify",
       },
       {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs",
       },
       {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
+        text: "Articles",
+        href: "https://medium.com/vuetify",
       },
     ],
     logo,
     whatsNext: [
       {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com',
+        text: "Explore components",
+        href: "https://vuetifyjs.com",
       },
       {
-        text: 'Roadmap',
-        href: 'https://vuetifyjs.com/introduction/roadmap/',
+        text: "Roadmap",
+        href: "https://vuetifyjs.com/introduction/roadmap/",
       },
       {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
       },
     ],
   }),
-}
+};
 </script>
